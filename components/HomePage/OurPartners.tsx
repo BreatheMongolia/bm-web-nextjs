d5a39fe4730540fa505917378958e79c84abdb94
Nara Davaasuren <94732814+Nara1469@users.noreply.github.com>
2023-05-19 14:32:13 -0600

    Added react-slick package

--- components/HomePage/OurPartners.tsx
+++ components/HomePage/OurPartners.tsx
@@ -1,5 +1,6 @@
 import { H2 } from 'components/generic/Typography'
 import { Page_Customfields_PartnersLogos } from 'graphql/generated'
+import Slider from "react-slick"
 
 export const OurPartners = ({
   title,
@@ -14,14 +15,54 @@ export const OurPartners = ({
   // console.log(title, partnerLogos)
   // TODO: Add <Slider /> component (react-slick)
   // TODO: Div/styling for images within Slider
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
+
   return (
     <div>
       <H2 title={title.mn} />
-      <div>
-        {partnerLogos.map((x, idx) => {
-          return <div key={idx}>{x.partnersLogosUrls}</div>
-        })}
-      </div>
+      {/* <Slider {...settings}> */}
+          {partnerLogos.map((x, idx) => {
+            return <div key={idx}>{x.partnersLogosImage.mediaItemUrl}</div>
+          })}
+      {/* </Slider> */}
     </div>
   )
 }
