2cc82182d24bb9bd5863361198033df84d4c12c4
Uchral Batkhuyag <uchral@breathemongolia.org>
2023-05-19 13:15:30 -0700

    join Breathe Mongolia on homepage

--- components/HomePage/JoinBMSection.tsx
+++ components/HomePage/JoinBMSection.tsx
@@ -1,8 +1,9 @@
 import { H2 } from 'components/generic/Typography'
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import parse from "html-react-parser";
 
 import {
   Page_Customfields_CountriesInfoText,
@@ -23,50 +24,54 @@ export const JoinBMSection = ({
   // TODO: Slider for the slider images
   const firstImageLink = slider[0].sliderImageLink;
   const secondImageLink = slider[1].sliderImageLink;
-
-  const ImageSlider = () => {
-    const sliderRef = useRef<Slider>(null);
   
-    useEffect(() => {
-      const interval = setInterval(() => {
-        if (sliderRef.current) {
-          const currentSlide = sliderRef.current.innerSlider.state.currentSlide;
-          if (currentSlide === 0) {
-            sliderRef.current.slickGoTo(1);
-          }
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 15000,
+    cssEase: "linear",
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          dots: true
         }
-      }, 15000);
-  
-      return () => clearInterval(interval);
-    }, []);
-  
-    const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      initialSlide: 0,
-    };
-  
-    return (
-      <Slider {...settings} ref={sliderRef}>
-        <div>
-          <img src={firstImageLink} alt="Photo 1" />
-        </div>
-        <div>
-          <img src={secondImageLink} alt="Photo 2" />
-        </div>
-      </Slider>
-    );
-  };
-  
-
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
   // TODO: show countries info section
+  const firstCountriesInfo = countriesInfoText[0];
+  const secondCountriesInfo = countriesInfoText[1];
+  const thirdCountriesInfo = countriesInfoText[2];
   // TODO: Show volunteer positions section
   // - Blocked: By API call for volunteer position, but show the UI for now
-  console.log(`This is in console: ${firstImageLink}`);
-  console.log(`This is in console: ${secondImageLink}`);
+
+  // console.log(`This is in console: ${firstCountriesInfo.infoIcon}`);
+  
+  // console.log(`This is in console: ${secondImageLink}`);
   // TEMP: object to test the Volunteers temp section
   const volunteersTemp = [
     {
@@ -78,7 +83,29 @@ export const JoinBMSection = ({
     <div>
       <H2 title={title.mn} descriptionHtml={descriptionHtml.mn} />
       {/* {volunteersTemp.map(x)} */}
-      <ImageSlider />
+      <Slider {...settings}>
+        <div>
+          <img src={firstImageLink} alt="Photo 1" />
+        </div>
+        <div>
+          <img src={secondImageLink} alt="Photo 2" />
+        </div>
+      </Slider>
+      <div className="first text">
+        <img src={firstCountriesInfo.infoIcon} alt="" />
+        {parse(firstCountriesInfo.customTextMn)}
+        {firstCountriesInfo.fieldGroupName}
+      </div>
+      <div className="second text">
+        <img src={secondCountriesInfo.infoIcon} alt="" />
+        {parse(secondCountriesInfo.customTextMn)}
+        {secondCountriesInfo.fieldGroupName}
+      </div>
+      <div className="third text">
+        <img src={thirdCountriesInfo.infoIcon} alt="" />
+        {parse(thirdCountriesInfo.customTextMn)}
+        {thirdCountriesInfo.fieldGroupName}
+      </div>
     </div>
   )
 }
