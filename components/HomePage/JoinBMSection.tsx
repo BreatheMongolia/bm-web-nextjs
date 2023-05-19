d7ad81355e94144bcd57f86106c6722ebb2a18ec
Uchral Batkhuyag <uchral@breathemongolia.org>
2023-05-19 11:40:42 -0700

    Photo slide

--- components/HomePage/JoinBMSection.tsx
+++ components/HomePage/JoinBMSection.tsx
@@ -1,4 +1,9 @@
 import { H2 } from 'components/generic/Typography'
+import React, { useEffect, useRef } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import {
   Page_Customfields_CountriesInfoText,
   Page_Customfields_JoinBreatheMongoliaImageSlider,
@@ -16,10 +21,52 @@ export const JoinBMSection = ({
   countriesInfoText: Page_Customfields_CountriesInfoText[]
 }) => {
   // TODO: Slider for the slider images
+  const firstImageLink = slider[0].sliderImageLink;
+  const secondImageLink = slider[1].sliderImageLink;
+
+  const ImageSlider = () => {
+    const sliderRef = useRef<Slider>(null);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        if (sliderRef.current) {
+          const currentSlide = sliderRef.current.innerSlider.state.currentSlide;
+          if (currentSlide === 0) {
+            sliderRef.current.slickGoTo(1);
+          }
+        }
+      }, 15000);
+  
+      return () => clearInterval(interval);
+    }, []);
+  
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 0,
+    };
+  
+    return (
+      <Slider {...settings} ref={sliderRef}>
+        <div>
+          <img src={firstImageLink} alt="Photo 1" />
+        </div>
+        <div>
+          <img src={secondImageLink} alt="Photo 2" />
+        </div>
+      </Slider>
+    );
+  };
+  
+
   // TODO: show countries info section
   // TODO: Show volunteer positions section
   // - Blocked: By API call for volunteer position, but show the UI for now
-
+  console.log(`This is in console: ${firstImageLink}`);
+  console.log(`This is in console: ${secondImageLink}`);
   // TEMP: object to test the Volunteers temp section
   const volunteersTemp = [
     {
@@ -31,6 +78,7 @@ export const JoinBMSection = ({
     <div>
       <H2 title={title.mn} descriptionHtml={descriptionHtml.mn} />
       {/* {volunteersTemp.map(x)} */}
+      <ImageSlider />
     </div>
   )
 }
