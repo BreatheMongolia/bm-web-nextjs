b093c9df1f65424b02cd422eb4a97e64057bf9d5
Uchral Batkhuyag <uchral@breathemongolia.org>
2023-05-23 11:53:24 -0700

    Join Breathe Mongolia section on homepage

--- components/HomePage/JoinBMSection.tsx
+++ components/HomePage/JoinBMSection.tsx
@@ -64,42 +64,68 @@ export const JoinBMSection = ({
     ]
   }
 
+  const volunteersTemp = [
+    {
+      title: 'Test Volunteer Position',
+      url: 'https://www.notion.so/breathemongolia/Fundraising-Manager-3c5a7d35aaad4b92939eaab909d270e4',
+    },
+  ]
+
   // TODO: show countries info section
   // TODO: Show volunteer positions section
   // - Blocked: By API call for volunteer position, but show the UI for now
 
-  // console.log(`This is in console: ${firstCountriesInfo.infoIcon}`);
 
-  // console.log(`This is in console: ${secondImageLink}`);
+  // console.log(`This is in console: ${descriptionHtml.mn}`);
   // TEMP: object to test the Volunteers temp section
-  const renderSlides = () =>
-    slider.map(x => (
-      <div>
-        <img src={x.sliderImage.mediaItemUrl} />
-      </div>
-    ));
 
   return (
-    <div className='join-bm'>
+    <div className="join-bm-slider-wrapper custom-sections-gap">
+      <div className="home-main-content">
       <H2 title={title.mn} descriptionHtml={descriptionHtml.mn} />
-      <div className='join-bm-section'>
-        <div className='photo-slider'>
-          <Slider {...settings}>
-            {renderSlides()}
-          </Slider>
-        </div>
-        <div className='volunteer-info'>
-          <div className='volunteer-count'>
-            {countriesInfoText.map(x => {
-              return (
-                <div className='country-text'>
-                  <img className='volunteer-hours' src={x.infoIcon.mediaItemUrl} />
-                  {x.customTextMn && <div className="text-data-custom">{parse(x.customTextMn)}</div>}
+        <div className="join_bm_carousel_sec">
+            <div className="membersPhoto">
+              <Slider {...settings}>
+                {slider.map(x => (
+      <div key={x.sliderImage.databaseId}>
+        <img className='slider-image' src={x.sliderImage.mediaItemUrl} />
+      </div>
+    ))}
+              </Slider>
+            </div>
+          <div className="volunteer-count">
+            <div className="volunteer-count-section  flex-row">
+              {countriesInfoText.map(x => (
+                <div className="country_count_col" key={x.infoIcon.databaseId}>
+                  <div className="text-center country-text">
+                    <img src={x.infoIcon.mediaItemUrl} alt="" />
+                    {x.customTextMn && <div className="text-data-custom">{parse(x.customTextMn)}</div>}
+                  </div>
                 </div>
-              )
-            })}
-          </div>
-          <div className='volunteer-opportunity'>
+              ))}
+            </div>
+
+            <div className="opportunity-section">
+              {/* <p className="title">{t("home.joinBm.opportunities")}</p> */}
+              <ul className="position-list">
+                {volunteersTemp.map((x, idx) => (
+                  <li className="position_list_el" key={idx}>
+                    <a className="pos_link_url" href={x.url} target="_blank">
+                      {/* {getTranslated(
+                        volunteerPos?.node?.customFields?.position,
+                        volunteerPos?.node?.customFields?.positionMn
+                      )} */}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div className="apply_button_container">
+                <a className="apply_now_button" href="https://link.breathemongolia.org/volunteer" target="_blank">
+                  {/* {t("home.joinBm.applyNow")} */}
+                  Apply Now
+                </a>
+              </div>
+            </div>
 
           </div>
         </div>
