99365c39808d7570e21213741e99a67fc6e20624
tgldr <rbktgldr@gmail.com>
2023-06-09 16:14:22 -0500

    Update date format

--- components/HomePage/OurWorkCarousel.tsx
+++ components/HomePage/OurWorkCarousel.tsx
@@ -1,12 +1,12 @@
 import { H2 } from 'components/generic/Typography'
-import React, { FC } from "react"
+import React, { FC } from 'react'
 import { Page_Customfields_CampaignAndOurWorkSlider } from 'graphql/generated'
 import { useTranslation } from 'next-i18next'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Slider from 'react-slick'
-import parse from "html-react-parser"
-import dayjs from "dayjs"
+import parse from 'html-react-parser'
+import dayjs from 'dayjs'
 import Arrow from 'components/generic/Arrow'
 import SliderLeftArrow from 'assets/icons/SliderLeftArrow'
 import SliderRightArrow from 'assets/icons/SliderRightArrow'
@@ -18,12 +18,12 @@ export const OurWorkCarousel = ({
   campaigns: Page_Customfields_CampaignAndOurWorkSlider[]
 }) => {
   const { t } = useTranslation('home')
-  function formatMyDate(value: any, locale = "fr-Fr") {
-    const year = new Date(value).toLocaleDateString(locale, { year: "numeric" })
-    const month = new Date(value).toLocaleDateString(locale, { month: "numeric" })
-    const day = new Date(value).toLocaleDateString(locale, { day: "numeric" })
-    return day + " • " + month + " • " + year
+
+  function formatMyDate(value: string) {
+    if (!value) return <></>
+    return dayjs(value).format('DD • MM • YYYY')
   }
+
   const settings = {
     dots: false,
     infinite: false,
@@ -33,7 +33,7 @@ export const OurWorkCarousel = ({
     arrows: true,
     autoplay: false,
     autoplaySpeed: 5000,
-    cssEase: "linear",
+    cssEase: 'linear',
     adaptiveHeight: true,
     centerMode: false,
     variableWidth: true,
@@ -43,8 +43,8 @@ export const OurWorkCarousel = ({
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: false
-        }
+          infinite: false,
+        },
       },
       {
         breakpoint: 600,
@@ -52,8 +52,8 @@ export const OurWorkCarousel = ({
           slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 1,
-          infinite: false
-        }
+          infinite: false,
+        },
       },
       {
         breakpoint: 480,
@@ -62,13 +62,15 @@ export const OurWorkCarousel = ({
           slidesToScroll: 1,
           infinite: false,
           centerMode: false,
-          variableWidth: true
-        }
-      }
-    ]
+          variableWidth: true,
+        },
+      },
+    ],
   }
-  const sortedCampaigns = campaigns.sort((a: any, b: any) =>
-    dayjs(a?.campaignDate).isBefore(dayjs(b?.campaignDate)) ? 1 : -1
+
+  const sortedCampaigns = campaigns.sort(
+    (a: Page_Customfields_CampaignAndOurWorkSlider, b: Page_Customfields_CampaignAndOurWorkSlider) =>
+      dayjs(a.campaignDate).isBefore(dayjs(b.campaignDate)) ? 1 : -1,
   )
   return (
     <div className="campaign-slider-wrapper custom-sections-gap">
@@ -80,44 +82,50 @@ export const OurWorkCarousel = ({
           url: '/news',
         }}
       />
-      <Slider {...settings}
-      prevArrow={
-        <Arrow check={0} classes="prev-gray-arrow">
-          <SliderLeftArrow />
-        </Arrow>
-      }
-      nextArrow={
-        <Arrow check={sortedCampaigns.length - 1} classes="next-gray-arrow">
-          <SliderRightArrow />
-        </Arrow>
-      }
+      <Slider
+        {...settings}
+        prevArrow={
+          <Arrow check={0} classes="prev-gray-arrow">
+            <SliderLeftArrow />
+          </Arrow>
+        }
+        nextArrow={
+          <Arrow check={sortedCampaigns.length - 1} classes="next-gray-arrow">
+            <SliderRightArrow />
+          </Arrow>
+        }
       >
         {sortedCampaigns.map(campaign => (
-        <React.Fragment key={campaign.volunteerImage.id}>
-        <div
-          key={campaign.volunteerImage.id}
-          className="campaignCard"
-          onClick={() => window.open(campaign.campaignCatgeoryUrl, "_blank")}
-        >
-          <img className="slider-image" src={campaign.volunteerImage?.mediaDetails.sizes !== null
-                ? campaign?.volunteerImage?.mediaDetails.sizes[0].sourceUrl
-                : ""} />
-          <div className="campaign-content">
-            <div className="campaignCategory">
-              <span className="custom_dot_green"></span>
-              <span className="custom_green_span"> {campaign.campaignCategoryTextMn}</span>
+          <React.Fragment key={campaign.volunteerImage.id}>
+            <div
+              key={campaign.volunteerImage.id}
+              className="campaignCard"
+              onClick={() => window.open(campaign.campaignCatgeoryUrl, '_blank')}
+            >
+              <img
+                className="slider-image"
+                src={
+                  campaign.volunteerImage?.mediaDetails.sizes !== null
+                    ? campaign?.volunteerImage?.mediaDetails.sizes[0].sourceUrl
+                    : ''
+                }
+              />
+              <div className="campaign-content">
+                <div className="campaignCategory">
+                  <span className="custom_dot_green"></span>
+                  <span className="custom_green_span"> {campaign.campaignCategoryTextMn}</span>
+                </div>
+                <h3 className="campaign-title">{campaign.campaignTitleMn}</h3>
+                {campaign?.campaignDescription && (
+                  <div className="campaign-desc">{parse(campaign.campaignDescriptionMn)}</div>
+                )}
+                <div className="campaign-date">
+                  {' '}
+                  <span>{formatMyDate(campaign.campaignDate)} </span>
+                </div>
+              </div>
             </div>
-            <h3 className="campaign-title">{campaign.campaignTitleMn}</h3>
-            {campaign?.campaignDescription && (
-              <div className="campaign-desc">{parse(campaign.campaignDescriptionMn)}</div>
-            )}
-            <div className="campaign-date">
-              {" "}
-              <span>{formatMyDate(campaign.campaignDate)} </span>
-            </div>
-          </div>
-        </div>
-        </React.Fragment>
+          </React.Fragment>
         ))}
       </Slider>
     </div>
