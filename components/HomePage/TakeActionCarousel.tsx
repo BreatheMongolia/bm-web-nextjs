d5763c8b7cf49824dd04c8c099ddc79dbe595622
Nara Davaasuren <94732814+Nara1469@users.noreply.github.com>
2023-06-09 15:45:37 -0600

    Added local change

--- components/HomePage/TakeActionCarousel.tsx
+++ components/HomePage/TakeActionCarousel.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { H2 } from 'components/generic/Typography'
-import { Page_Customfields_FeaturedTakeActions } from 'graphql/generated'
+import { TakeAction, Page_Customfields_FeaturedTakeActions } from 'graphql/generated'
 import Slider from 'react-slick'
-import { TakeActionCard } from '../Cards/TakeActionCard'
-import { TakeAction } from 'graphql/generated'
 import { useTranslation } from 'next-i18next'
-// import { useTranslation } from 'react-i18next'
+import Arrow from 'components/generic/Arrow'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
+// import { TakeActionCard } from '../Cards/TakeActionCard'
+// import { TakeAction } from 'graphql/generated'
 // import { useHistory } from "react-router-dom"
-// import SliderLeftArrow from "../assets/icons/SliderLeftArrow"
-// import SliderRightArrow from "../assets/icons/SliderRightArrow"
+
+export type TakeActionArray = {
+  title: string
+  typeOfAction: []
+  featuredImage: string
+}
 
 export const TakeActionCarousel = ({
   title,
@@ -18,14 +23,55 @@ export const TakeActionCarousel = ({
   takeActionPosts: Page_Customfields_FeaturedTakeActions[]
 }) => {
   const { t } = useTranslation('home')
-  // console.log(takeActionPosts);
-  // const { t } = useTranslation()
+  console.log(takeActionPosts);
+
+  // const featuredImageBox = getImage(
+  //   takeAction.featuredImage.node?.mediaDetails,
+  //   takeAction.featuredImage.node?.mediaItemUrl,
+  //   takeAction.featuredImage.node?.mediaDetails,
+  //   'medium_large',
+  // )
+  // const getTransformedData = (takeActionPosts: Page_Customfields_FeaturedTakeActions[]) => {
+  //   if (takeActionPosts.length === 0) {
+  //     return []
+  //   }  
+  //   const takeActions: TakeActionArray[] = []
+  //   takeActionPosts.map((x: TakeAction) => {
+  //     takeActions.push({
+  //       title: x?.customFields?.titleMn,
+  //         // getTranslated(x?.customFields?.title, x?.customFields?.titleMn) !== null
+  //         //   ? getTranslated(x?.customFields?.title, x?.customFields?.titleMn)
+  //         //   : "",
+  //       typeOfAction: x?.customFields?.typeOfAction?.map(
+  //         (type: {
+  //           // databaseId: number
+  //           // actionTypeId: number
+  //           // name: string
+  //           // link: string
+  //           customFields: { name: string; nameMn: string }
+  //         }) => {
+  //           return {
+  //             ...type,
+  //             name: type?.customFields?.nameMn
+  //             // getTranslated(type?.customFields?.name, type?.customFields?.nameMn)
+  //           }
+  //         }
+  //       ),
+  //       featuredImage:
+  //         x?.featuredImage?.node?.mediaDetails.sizes !== null
+  //           ? x?.featuredImage?.node?.mediaDetails.sizes[0].sourceUrl
+  //           : ""
+  //     })
+  //   }
+  //   return takeActions
+  // }
+
   // Styling the settings for take-action-carousel within Slider
   const settings = {
     dots: false,
     infinite: false,
     speed: 800,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     autoplaySpeed: 5000,
@@ -37,7 +83,7 @@ export const TakeActionCarousel = ({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 2,
           infinite: true,
           dots: true,
@@ -46,7 +92,7 @@ export const TakeActionCarousel = ({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 2,
         },
@@ -62,32 +108,51 @@ export const TakeActionCarousel = ({
   }
 
   return (
-    <div className="custom-sections-gap take-action-carousel-section">
+    <div className="take-action-carousel-section">
       <H2
-        // title={t('home.take-actions.title')}
-        title="Take Action"
+        title={t('take-action.title')}
         trailingLineColor="yellow"
         extraButton={{
-          // title: t('home.take-actions.seeMore'),
-          title: 'All',
+          title: t('take-actions.seeMore'),
           url: '/take-actions',
         }}
       />
       <Slider
         {...settings}
-        // prevArrow={
-        //   <Arrow check={0} classes="prev-gray-arrow">
-        //     <SliderLeftArrow />
-        //   </Arrow>
-        // }
-        // nextArrow={
-        //   <Arrow check={takeActions?.length - 4} classes="next-gray-arrow">
-        //     <SliderRightArrow />
-        //   </Arrow>
-        // }
+        prevArrow={
+          <Arrow check={0} classes="prev-gray-arrow">
+            <ChevronLeftIcon className="w-8 h-8 text-white" />
+          </Arrow>
+        }
+        nextArrow={
+          <Arrow check={takeActionPosts?.length - 3} classes="next-gray-arrow">
+            <ChevronRightIcon className="w-8 h-8 text-white" />
+          </Arrow>
+        }
       >
         {takeActionPosts.map((x, idx) => (
-          <div>{x.featuredImage != null && <TakeActionCard key={idx} takeAction={x} />}</div>
+          <React.Fragment>
+            <div
+              key={idx}
+              className="relative transition-all bg-slate-300 rounded-md overflow-hidden cursor-pointer h-52 bg-cover bg-center group "
+              onClick={() => window.open(x.featuredImage.node?.mediaItemUrl, '_blank')}
+            >
+              <div className="flex flex-col h-full justify-end bg-gradient-to-t from-black/80 to-black/0 via-black/30 group-hover:from-black/90 group-hover:to-black/20 transition-all">
+                <img
+                  className="object-fill max-w-none h-full"
+                  src={x?.featuredImage?.node?.mediaDetails.sizes !== null
+                    ? x?.featuredImage?.node?.mediaDetails.sizes[0].sourceUrl
+                    : ''}
+                />
+                <div className="w-full p-4 ">
+                  <div className="w-full text-white line-clamp-2">
+                    {' '}
+                    {x.customFields.typeOfAction[0].customFields.nameMn}{' '}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </React.Fragment>
         ))}
       </Slider>
     </div>
