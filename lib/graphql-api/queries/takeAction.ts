f6b72f4abbb805ae973c6f1fee2e6063eb257f30
Uchral <uchbatkhuyag@gmail.com>
2023-06-28 11:59:18 -0700

    add: take action API

--- lib/graphql-api/queries/takeAction.ts
+++ lib/graphql-api/queries/takeAction.ts
@@ -1,10 +1,59 @@
 import { fetchAPI } from 'lib/graphql-api/api'
-import { Page, PageIdType, MediaItemSizeEnum } from 'graphql/generated'
+import { Page, PageIdType } from 'graphql/generated'
 
 export async function getTakeActionsPage(id: string, idType: PageIdType = PageIdType.Uri): Promise<Page> {
-  // TODO: fetch take actions api
-
-  // const data = fetchAPI()
-  // return {}
-  return null
+  const data = await fetchAPI(
+    `
+      query page($id: ID!, $idType: PageIdType!) {
+          page(id: $id, idType: $idType) {
+              title
+              dateGmt
+              customFields {
+                  ${TakeActionGQLQuerySections.takeAction}
+              }
+          }
+      }
+    `,
+    {
+      variables: { id, idType },
+    },
+  )
+  return data.page
+}
+const TakeActionGQLQuerySections = {
+  takeAction: `
+    additionalResources {
+      title
+      titleMn
+      url
+      urlMn
+    }
+    introductionText
+    introductionTextMn
+    pledgeTitle
+    pledgeTitleMn
+    pledgeContent
+    pledgeContentMn
+    titleMn
+    title
+    typeOfAction {
+      customFields {
+        name
+        nameMn
+      }
+    }
+    listOfPhotos {
+      mediaItemUrl
+      caption
+    }
+    listOfVideos {
+      videoLink
+    }
+    listOfSubSections {
+      title
+      titleMn
+      bodyMn
+      body
+    }
+    `,
 }
