a3c663d8a76fb805bd2959115825d1f7b39f0f5c
tgldr <rbktgldr@gmail.com>
2023-07-07 16:13:25 -0500

    Update take actions API

--- lib/graphql-api/queries/takeAction.ts
+++ lib/graphql-api/queries/takeAction.ts
@@ -1,17 +1,49 @@
 import { fetchAPI } from 'lib/graphql-api/api'
-import { Page, PageIdType } from 'graphql/generated'
+import { Page, PageIdType, TakeAction } from 'graphql/generated'
 
-export async function getTakeActionsPage(id: string, idType: PageIdType = PageIdType.Uri): Promise<Page> {
+export async function getTakeActionsDetail(id): Promise<Page> {
   const data = await fetchAPI(
-    `
-      query page($id: ID!, $idType: PageIdType!) {
+    `query GetTakeActionById($id: [ID]) {
+      takeActions(where: { in: $id }) {
+        edges {
+          node {
+            ${TakeActionGQLQuerySections.takeActionDetail}
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: { id },
+    },
+  )
+  return data.takeActions.edges[0].node || {}
+}
+
+export async function getTakeActionsLatest() {
+  const data = await fetchAPI(
+    `query getLatestTakeActions {
+      takeActions(where: { orderby: { order: DESC, field: DATE } }, first: 9) {
+        edges {
+          node {
+            ${TakeActionGQLQuerySections.takeAction}
+            }
+          }
+        }
+      }
+    `,
+  )
+
+  return data.takeActions?.edges || []
+}
+
+export async function getFeaturedTakeActions(id: string, idType: PageIdType = PageIdType.Uri): Promise<Page> {
+  const data = await fetchAPI(
+    `query page($id: ID!, $idType: PageIdType!) {
           page(id: $id, idType: $idType) {
-              title
-              dateGmt
               customFields {
-                  ${TakeActionGQLQuerySections.takeAction}
+                  ${TakeActionGQLQuerySections.featuredTakeAction}
               }
-              totalPledges
           }
       }
     `,
@@ -19,42 +51,142 @@ export async function getTakeActionsPage(id: string, idType: PageIdType = PageId
       variables: { id, idType },
     },
   )
-  return data.page
+  return data.page?.customFields?.featuredTakeActionsLanding || []
 }
-const TakeActionGQLQuerySections = {
-  takeAction: `
-    additionalResources {
-      title
-      titleMn
-      url
-      urlMn
-    }
-    introductionText
-    introductionTextMn
-    pledgeTitle
-    pledgeTitleMn
-    pledgeContent
-    pledgeContentMn
-    titleMn
-    title
-    typeOfAction {
-      customFields {
-        name
-        nameMn
+
+export async function getTakeActionSlugs(): Promise<TakeAction[]> {
+  const data = await fetchAPI(
+    `
+    query getAllTakeActions {
+      takeActions(first: 1000) {
+        edges {
+          node {
+            databaseId
+            desiredSlug
+            slug
+            dateGmt
+          }
+        }
       }
     }
-    listOfPhotos {
-      mediaItemUrl
-      caption
-    }
-    listOfVideos {
-      videoLink
-    }
-    listOfSubSections {
-      title
-      titleMn
-      bodyMn
-      body
-    }
     `,
+  )
+  return data.takeActions && data.takeActions.edges ? data.takeActions.edges.map(x => x.node as TakeAction) : []
+}
+
+const TakeActionGQLQuerySections = {
+  featuredTakeAction: `
+    featuredTakeActionsLanding {
+      ... on TakeAction {
+        databaseId
+        dateGmt
+        customFields {
+          additionalResources {
+            title
+            titleMn
+            url
+            urlMn
+          }
+          introductionText
+          introductionTextMn
+          pledgeContent
+          pledgeContentMn
+          titleMn
+          title
+          excerpt
+          excerptMn
+          typeOfAction {
+            customFields {
+              name
+              nameMn
+            }
+          }
+          listOfPhotos {
+            mediaItemUrl
+            caption
+          }
+          listOfVideos {
+            videoLink
+          }
+          listOfSubSections {
+            title
+            titleMn
+            bodyMn
+            body
+          }
+        }
+        featuredImage {
+          node {
+            mediaItemUrl
+          }
+        }
+      }
+    }
+  `,
+  takeAction: `
+      databaseId
+      date
+      dateGmt
+      customFields {
+        titleMn
+        title
+        typeOfAction {
+          customFields {
+            name
+            nameMn
+          }
+        }
+      }
+      featuredImage {
+        node {
+          id
+          mediaItemUrl
+          mediaDetails {
+            sizes(include: MEDIUM) {
+              height
+              width
+              sourceUrl
+            }
+          }
+        }
+      }
+    `,
+  takeActionDetail: `
+    databaseId
+    dateGmt
+    customFields {
+      additionalResources {
+        title
+        titleMn
+        url
+        urlMn
+      }
+      introductionText
+      introductionTextMn
+      pledgeTitle
+      pledgeTitleMn
+      pledgeContent
+      pledgeContentMn
+      titleMn
+      title
+      typeOfAction {
+        customFields {
+          name
+          nameMn
+        }
+      }
+      listOfPhotos {
+        mediaItemUrl
+        caption
+      }
+      listOfVideos {
+        videoLink
+      }
+      listOfSubSections {
+        title
+        titleMn
+        bodyMn
+        body
+      }
+    }`,
 }
---
+++ pages/take-actions/[slug].tsx
@@ -0,0 +1,64 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Head from 'next/head'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { getNewsFull, getNewsPostSlugs } from 'lib/graphql-api/queries/news'
+import { TakeAction } from 'graphql/generated'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import { getTakeActionSlugs, getTakeActionsDetail } from 'lib/graphql-api/queries/takeAction'
+
+interface TakeActionPageProps {
+  post: TakeAction
+}
+
+export default function TakeActionPostPage({ takeAction }) {
+  const router = useRouter()
+
+  if (!router.isFallback && !takeAction?.databaseId) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  const { t } = useTranslation('common')
+
+  return (
+    <div>
+      {router.isFallback ? (
+        <>Loading</>
+      ) : (
+        <>
+          <article>
+            <Head>
+              <title>{`${takeAction.customFields.title} - Breathe Mongolia Clean Air Coalition`}</title>
+            </Head>
+            <div className="container max-w-screen-lg">
+              <h2>{takeAction.customFields.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: takeAction.customFields.introductionText }}></div>
+            </div>
+          </article>
+        </>
+      )}
+    </div>
+  )
+}
+
+export const getStaticProps = async ({ params, locale }) => {
+  const takeAction = await getTakeActionsDetail(params?.slug)
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['home', 'nav', 'footer', 'map'])),
+      takeAction,
+    },
+    revalidate: 60,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async ({}) => {
+  const takeActions = await getTakeActionSlugs()
+
+  return {
+    paths: takeActions.map((x, idx) => `/take-actions/${x.desiredSlug || x.slug || x.databaseId}`) || [],
+    fallback: true,
+  }
+}
