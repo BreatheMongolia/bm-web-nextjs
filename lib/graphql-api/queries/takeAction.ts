7b5940a56dad4962399c2b27f7409967f59bee8f
tgldr <rbktgldr@gmail.com>
2023-07-07 14:03:04 -0500

    Update take actions query

--- lib/graphql-api/queries/takeAction.ts
+++ lib/graphql-api/queries/takeAction.ts
@@ -1,15 +1,48 @@
 import { fetchAPI } from 'lib/graphql-api/api'
-import { TakeAction, TakeActionIdType } from 'graphql/generated'
+import { Page, PageIdType, TakeAction } from 'graphql/generated'
 
-export async function getTakeActionsPage(id: string, idType: TakeActionIdType = TakeActionIdType.Uri): Promise<TakeAction> {
+export async function getTakeActionsDetail(id): Promise<Page> {
   const data = await fetchAPI(
-    `
-      query takeAction($id: ID!, $idType: TakeActionIdType!) {
-          takeAction(id: $id, idType: $idType) {
-              title
-              dateGmt
+    `query GetTakeActionById($id: [ID]) {
+      takeActions(where: { in: $id }) {
+        edges {
+          node {
+            ${TakeActionGQLQuerySections.takeActionDetail}
+            }
+          }
+        }
+      }
+    `,
+    {
+      variables: { id },
+    },
+  )
+  return data.takeActions.edges[0].node || {}
+}
+
+export async function getTakeActionsLatest() {
+  const data = await fetchAPI(
+    `query getLatestTakeActions {
+      takeActions(where: { orderby: { order: DESC, field: DATE } }, first: 9) {
+        edges {
+          node {
+            ${TakeActionGQLQuerySections.takeAction}
+            }
+          }
+        }
+      }
+    `,
+  )
+
+  return data.takeActions?.edges || []
+}
+
+export async function getFeaturedTakeActions(id: string, idType: PageIdType = PageIdType.Uri): Promise<Page> {
+  const data = await fetchAPI(
+    `query page($id: ID!, $idType: PageIdType!) {
+          page(id: $id, idType: $idType) {
               customFields {
-                  ${TakeActionGQLQuerySections.takeActionPosts}
+                  ${TakeActionGQLQuerySections.featuredTakeAction}
               }
           }
       }
@@ -18,42 +51,142 @@ export async function getTakeActionsPage(id: string, idType: TakeActionIdType = 
       variables: { id, idType },
     },
   )
-  return data.takeAction
+  return data.page?.customFields?.featuredTakeActionsLanding || []
 }
-const TakeActionGQLQuerySections = {
-  takeActionPosts: `
-    additionalResources {
-      title
-      titleMn
-      url
-      urlMn
-    }
-    introductionText
-    introductionTextMn
-    pledgeContent
-    pledgeContentMn
-    titleMn
-    title
-    excerpt
-    excerptMn
-    typeOfAction {
-      customFields {
-        name
-        nameMn
+
+export async function getTakeActionSlugs(): Promise<TakeAction[]> {
+  const data = await fetchAPI(
+    `
+    query getAllTakeActions {
+      takeActions(first: 1000) {
+        edges {
+          node {
+            databaseId
+            desiredSlug
+            slug
+            dateGmt
+          }
+        }
       }
     }
-    listOfPhotos {
-      mediaItemUrl
-      caption
-    }
-    listOfVideos {
-      videoLink
-    }
-    listOfSubSections {
-      title
-      titleMn
-      bodyMn
-      body
-    }
     `,
+  )
+  return data.takeActions && data.takeActions.edges ? data.takeActions.edges.map(x => x.node as TakeAction) : []
+}
+
+const TakeActionGQLQuerySections = {
+  featuredTakeAction: `
+    featuredTakeActionsLanding {
+      ... on TakeAction {
+        databaseId
+        dateGmt
+        customFields {
+          additionalResources {
+            title
+            titleMn
+            url
+            urlMn
+          }
+          introductionText
+          introductionTextMn
+          pledgeContent
+          pledgeContentMn
+          titleMn
+          title
+          excerpt
+          excerptMn
+          typeOfAction {
+            customFields {
+              name
+              nameMn
+            }
+          }
+          listOfPhotos {
+            mediaItemUrl
+            caption
+          }
+          listOfVideos {
+            videoLink
+          }
+          listOfSubSections {
+            title
+            titleMn
+            bodyMn
+            body
+          }
+        }
+        featuredImage {
+          node {
+            mediaItemUrl
+          }
+        }
+      }
+    }
+  `,
+  takeAction: `
+      databaseId
+      date
+      dateGmt
+      customFields {
+        titleMn
+        title
+        typeOfAction {
+          customFields {
+            name
+            nameMn
+          }
+        }
+      }
+      featuredImage {
+        node {
+          id
+          mediaItemUrl
+          mediaDetails {
+            sizes(include: MEDIUM) {
+              height
+              width
+              sourceUrl
+            }
+          }
+        }
+      }
+    `,
+  takeActionDetail: `
+    databaseId
+    dateGmt
+    customFields {
+      additionalResources {
+        title
+        titleMn
+        url
+        urlMn
+      }
+      introductionText
+      introductionTextMn
+      pledgeTitle
+      pledgeTitleMn
+      pledgeContent
+      pledgeContentMn
+      titleMn
+      title
+      typeOfAction {
+        customFields {
+          name
+          nameMn
+        }
+      }
+      listOfPhotos {
+        mediaItemUrl
+        caption
+      }
+      listOfVideos {
+        videoLink
+      }
+      listOfSubSections {
+        title
+        titleMn
+        bodyMn
+        body
+      }
+    }`,
 }
--- pages/take-actions/index.tsx
+++ pages/take-actions/index.tsx
@@ -1,6 +1,6 @@
 import { PageImageBanner } from 'components/generic/PageImageBanner'
 import { Page } from 'graphql/generated'
-import { getTakeActionsPage } from 'lib/graphql-api/queries/takeAction'
+import { getFeaturedTakeActions, getTakeActionsLatest } from 'lib/graphql-api/queries/takeAction'
 import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useRouter } from 'next/router'
@@ -8,7 +8,7 @@ import { useEffect } from 'react'
 import { useTranslation } from 'next-i18next'
 import { DonateSection, TakeActionsGrid } from 'components/TakeActionPage'
 
-const TakeActionsPage = ({ posts }: { posts: Page[] }) => {
+const TakeActionsPage = ({ latest, featured }) => {
   const { t } = useTranslation()
   const router = useRouter()
   const { i18n } = useTranslation()
@@ -21,7 +21,8 @@ const TakeActionsPage = ({ posts }: { posts: Page[] }) => {
     }
   }, [])
 
-  console.log(posts);
+  console.log(latest)
+  console.log(featured)
 
   return (
     <div>
@@ -33,7 +34,7 @@ const TakeActionsPage = ({ posts }: { posts: Page[] }) => {
       />
       <div className="container mx-auto flex flex-col gap-20">
         {/* <TakeActionsGrid takeAction={takeActions} /> */}
-        
+
         <DonateSection />
       </div>
     </div>
@@ -43,12 +44,14 @@ const TakeActionsPage = ({ posts }: { posts: Page[] }) => {
 export default TakeActionsPage
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const posts = await getTakeActionsPage('/')
+  const featured = await getFeaturedTakeActions('/')
+  const latest = await getTakeActionsLatest()
 
   return {
     props: {
       ...(await serverSideTranslations(locale ?? 'en', ['takeAction', 'nav', 'footer'])),
-      posts
+      featured,
+      latest,
     },
     revalidate: 60,
   }
