31c8e9dce28cf5f122e6617dba8a1adedf895608
Zulaa123 <zul221b@gmail.com>
2023-07-26 16:25:56 +0800

    feat: started about us base files
    - setup routing detection on page
    - setup empty component for info section
    - updated language files with subnavigation

---
+++ components/AboutUsPage/AboutUsInfoSection.tsx
@@ -0,0 +1,12 @@
+export const AboutUsInfoSection = () => {
+  return (
+    <div>
+      <div> Our Missions </div>
+      <div> What we do </div>
+      <div> why </div>
+      <div> vision </div>
+      {/* Leave for now, TODO: Utilize the Home OurPartners carousel */}
+      <div> partners and supporters </div>
+    </div>
+  )
+}
--- pages/about/[route].tsx
+++ pages/about/[route].tsx
@@ -1,17 +1,65 @@
-import { AboutUsHeader } from 'components/AboutUsPage'
+import { AboutUsHeader, AboutUsInfoSection } from 'components/AboutUsPage'
 import { GetStaticPaths } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const VALID_ROUTES = ['info', 'our-story', 'impact', 'our-team', 'support-us']
+const VALID_ROUTES = [
+  {
+    route: 'info',
+    title: 'subNavigationTabs.aboutUs',
+  },
+  {
+    route: 'our-story',
+    title: 'subNavigationTabs.aboutUs',
+  },
+  {
+    route: 'impact',
+    title: 'subNavigationTabs.aboutUs',
+  },
+  {
+    route: 'our-team',
+    title: 'subNavigationTabs.aboutUs',
+  },
+  {
+    route: 'support-us',
+    title: 'subNavigationTabs.aboutUs',
+  },
+]
 
 export default function AboutPageSection({}) {
   const router = useRouter()
+  const { t, i18n } = useTranslation('about')
 
+  const getAboutSectionByRoute = (route: string) => {
+    console.log(route)
+    switch (route) {
+      case '/about/info':
+        return <AboutUsInfoSection />
+      case '/about/impact':
+        return
+      default:
+        return <div> Not Found</div>
+    }
+  }
   return (
     <div>
       <AboutUsHeader />
-      <div>{router.asPath}</div>
+      {/* Tab Navbar */}
+      <div className="flex p-5 gap-4">
+        {VALID_ROUTES.map((x, idx) => {
+          return (
+            <div key={idx} className="flex flex-1 text-center w-full">
+              <Link href={`/about/${x.route}`} className="w-full hover:bg-rose-200 bg-rose-100">
+                <div className="w-full p-5">{t(x.title)}</div>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+      {/* Content */}
+      <div>{getAboutSectionByRoute(router.asPath)}</div>
     </div>
   )
 }
@@ -24,7 +72,7 @@ export const getStaticProps = async ({ locale }) => ({
 
 export const getStaticPaths: GetStaticPaths = async ({}) => {
   return {
-    paths: VALID_ROUTES.map(x => `/about/${x}`) || [],
+    paths: VALID_ROUTES.map(x => `/about/${x.route}`) || [],
     fallback: true,
   }
 }
