fd829f2b1e7a41ee7a55f92065312d0cca49a26e
Temuulen Erdenekhuu <temuulen.erdenekhuu@certik.com>
2023-10-10 23:30:49 -0500

    feat: more filters functionality

--- components/NewsPage/FullNewsGrid/index.tsx
+++ components/NewsPage/FullNewsGrid/index.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'next-i18next'
+import { Menu } from '@headlessui/react'
 import { NewsCard } from 'components/Cards'
 import { News } from 'graphql/generated'
 import { NewsGrid } from '../LandingPage'
-import { useEffect, useState } from 'react'
+import { FunnelIcon } from '@heroicons/react/24/solid'
 
 type Props = {
   news: News[]
@@ -33,11 +35,11 @@ const getUniqueCategories = (news: News[]) => {
 const CategoryButton = ({ name, category, onClick, isActive }) => {
   return (
     <div
-      className={`font-semibold px-4 py-1 cursor-pointer rounded-full text-[15px]
+      className={` px-4 py-2 cursor-pointer rounded-full text-[15px]
       ${
         isActive
-          ? 'bg-[#f09c4f] hover:opacity-80 text-white'
-          : 'bg-zinc-200 hover:bg-[#f09c4f] hover:text-white text-zinc-400'
+          ? 'bg-[#f09c4f] hover:opacity-80 text-white font-bold'
+          : 'bg-zinc-200 hover:bg-[#f09c4f] hover:text-white text-zinc-400 font-semibold'
       }`}
       onClick={() => {
         onClick(category)
@@ -59,10 +61,10 @@ const FullNewsGrid = ({ news }: Props) => {
 
   useEffect(() => {
     const categoriesToShow = uniqueCategories.filter(x => activeCategories.some(c => c.slug === x.slug))
-    console.log('here')
-    if (categoriesToShow.length < 5) {
+    const MIN_NUM_CATEGORIES = 3
+    if (categoriesToShow.length < MIN_NUM_CATEGORIES) {
       for (let i = 0; i < uniqueCategories.length; i++) {
-        if (categoriesToShow.length >= 5) {
+        if (categoriesToShow.length >= MIN_NUM_CATEGORIES) {
           break
         }
         const v = uniqueCategories[i]
@@ -71,11 +73,15 @@ const FullNewsGrid = ({ news }: Props) => {
         }
       }
     }
+
+    categoriesToShow.sort((a, b) => a.ctId - b.ctId)
+    setShownCategories(categoriesToShow)
+  }, [activeCategories])
+
+  useEffect(() => {
     if (activeCategories.length === 0) {
       setShowAll(true)
     }
-    categoriesToShow.sort((a, b) => a.ctId - b.ctId)
-    setShownCategories(categoriesToShow)
     // load the news
     const fNews = news.filter(x => {
       if (showAll) {
@@ -104,29 +110,63 @@ const FullNewsGrid = ({ news }: Props) => {
 
   return (
     <div>
-      <div className="flex gap-2 pt-5 pb-8">
-        <CategoryButton
-          name={t('all')}
-          category={null}
-          isActive={showAll}
-          onClick={() => {
-            setActiveCategory(null)
-          }}
-        />
-        {shownCategories.map((x, idx) => {
-          return (
-            <CategoryButton
-              name={i18n.language === 'mn' ? x.nameMn : x.name}
-              category={x}
-              key={idx}
-              isActive={activeCategories.some(c => c.id === x.id)}
-              onClick={cat => {
-                setActiveCategory(cat)
-              }}
-            />
-          )
-        })}
+      <div className="flex pt-5 pb-8 ">
+        <div className="flex gap-2 grow">
+          <CategoryButton
+            name={t('all')}
+            category={null}
+            isActive={showAll}
+            onClick={() => {
+              setActiveCategory(null)
+            }}
+          />
+          {shownCategories.map((x, idx) => {
+            return (
+              <CategoryButton
+                name={i18n.language === 'mn' ? x.nameMn : x.name}
+                category={x}
+                key={idx}
+                isActive={activeCategories.some(c => c.id === x.id)}
+                onClick={cat => {
+                  setActiveCategory(cat)
+                }}
+              />
+            )
+          })}
+        </div>
+        <div className="relative">
+          <Menu>
+            <Menu.Button className="bg-[#f09c4f] text-white font-bold text-[15px] py-2 px-4 rounded-full hover:opacity-80 active:opacity-80 flex gap-3 justify-center items-center">
+              {t('morefilter')}
+              <FunnelIcon className="h-4 w-4" />
+            </Menu.Button>
+            <Menu.Items className="absolute top-10 right-0 bg-white z-50 rounded border border-[#f09c4f] w-60">
+              {uniqueCategories.map((x, idx) => {
+                return (
+                  <Menu.Item>
+                    {() => {
+                      const isActive = activeCategories.some(c => c.id === x.id)
+                      return (
+                        <div
+                          className={`px-5 py-1 cursor-pointer border-b border-white hover:bg-[#f09c4f]/20 ${
+                            isActive ? 'text-[#f09c4f] bg-[#f09c4f]/20 font-semibold' : 'text-zinc-500'
+                          }`}
+                          onClick={() => {
+                            setActiveCategory(x)
+                          }}
+                        >
+                          {i18n.language === 'mn' ? x.nameMn : x.name}
+                        </div>
+                      )
+                    }}
+                  </Menu.Item>
+                )
+              })}
+            </Menu.Items>
+          </Menu>
+        </div>
       </div>
+
       <NewsGrid>
         {filteredNews.map((x, idx) => {
           return (
