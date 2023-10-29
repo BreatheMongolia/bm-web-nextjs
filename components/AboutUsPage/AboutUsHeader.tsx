95d5b6779d6927d423ac615340e90a37c478d53f
Nara Davaasuren <94732814+Nara1469@users.noreply.github.com>
2023-10-28 22:06:11 -0600

    Refactored the AboutUs InfoSection responsiveness

--- components/AboutUsPage/AboutUsHeader.tsx
+++ components/AboutUsPage/AboutUsHeader.tsx
@@ -30,27 +30,27 @@ export const AboutUsHeader = () => {
   const { t } = useTranslation('about')
 
   return (
-    <div>
-      <div className="grid sm:grid-cols-2 w-full">
+    <div className="flex flex-col">
+      <div className="grid sm:grid-cols-2 sm:grid-rows-1 w-full">
         {/* FIXME: Update the gradient color to match old */}
-        <div className="flex justify-center items-center bg-bm-blue bg-gradient-to-r from-bm-blue to-[#61b1ee] text-white">
-          <div className="text-left w-96">
+        <div className="hidden sm:flex sm:justify-center sm:items-center sm:bg-bm-blue sm:bg-gradient-to-r from-bm-blue to-[#61b1ee] text-white">
+          <div className="p-5 sm:w-96 text-left">
             <h1 className="text-3xl mb-5 font-bold">{t('header.title')}</h1>
             <h1 className="text-sm">{t('header.description')}</h1>
           </div>
         </div>
-        <div className="relative">
+        <div className="">
           <Image src={teamBreatheMongolia} alt="teamBreatheMongolia" width={1200} priority={true} />
         </div>
       </div>
 
       {/* Tab Navbar */}
-      <div className="tab-nav-container shadow-lg">
+      <div className="grid grid-cols-5 bg-white tab-nav-container shadow-lg">
         {VALID_ROUTES.map((x, idx) => {
           return (
             <div key={'routes' + idx} className="flex flex-1 text-center w-full">
               <Link href={`/about/${x.route}`} className="w-full hover:bg-gray-100">
-                <div className="tab-titles flex p-5">{t(x.title)}</div>
+                <div className="tab-titles py-5 px-2">{t(x.title)}</div>
               </Link>
             </div>
           )
--- components/AboutUsPage/AboutUsInfoSection.tsx
+++ components/AboutUsPage/AboutUsInfoSection.tsx
@@ -31,19 +31,25 @@ export const AboutUsInfoSection = () => {
   return (
     <div>
       <div className="flex flex-col">
+        <div className="sm:hidden flex justify-center items-center bg-bm-blue bg-gradient-to-r from-bm-blue to-[#61b1ee] text-white">
+          <div className="p-5 sm:w-96 text-center">
+            <h1 className="text-xl mb-5 font-bold">{t('header.title')}</h1>
+            <h1 className="text-sm">{t('header.description')}</h1>
+          </div>
+        </div>
         <div className="our_mission_background">
           <PollutionParticlesBackground />
           <div className="mission_vision_wrapper">
-            <div className="our_mission_title mb-2">{t('ourMission')}</div>
-            <div className="mission_paragraph mb-5">{t('missionStatement')}</div>
-            <div className="our_mission_title mb-2">{t('ourVision')}</div>
-            <div className="mission_paragraph mb-5">{t('visionStatement')}</div>
+            <div className="our_mission_title sm:mb-2">{t('ourMission')}</div>
+            <div className="mission_paragraph sm:mb-5">{t('missionStatement')}</div>
+            <div className="our_mission_title sm:mb-2">{t('ourVision')}</div>
+            <div className="mission_paragraph sm:mb-5">{t('visionStatement')}</div>
           </div>
         </div>
       </div>
-      <div className="what_we_do_container">
-        <h1 className="what_we_do_title">{t('whatWeDo')}</h1>
-        <p className="what_we_do_paragraph">{t('whatWeDoDescription')}</p>
+      <div className="what_we_do_container py-5">
+        <h1 className="what_we_do_title text-xl sm:text-3xl">{t('whatWeDo')}</h1>
+        <p className="what_we_do_paragraph my-5">{t('whatWeDoDescription')}</p>
         <div className="details_container">
           <div className="education_container">
             <EducationIcon />
@@ -62,34 +68,24 @@ export const AboutUsInfoSection = () => {
           </div>
         </div>
       </div>
-      <div className="why_container">
-        <h1 className="why">{t('why')}</h1>
-
-        <p className="why_details">{t('whyDescription')}</p>
-
-        <h1 className="alignment">{t('ourAlignment')}</h1>
-
-        <p className="alignment_details">{t('ourAlignmentDescription')}</p>
-
-        <div className="goal_images_container">
-          <div className="goal_images_one">
-            <img src="/images/Goal 3.png" width={130} height={130} />
-            <img src="/images/Goal 4.png" width={130} height={130} />
-            <img src="/images/Goal 5.png" width={130} height={130} />
-            <img src="/images/Goal 6.png" width={130} height={130} />
-          </div>
-          <div className="goal_images_two">
-            <img src="/images/Goal 7.png" width={130} height={130} />
-            <img src="/images/Goal 8.png" width={130} height={130} />
-            <img src="/images/Goal 9.png" width={130} height={130} />
-          </div>
+      <div className="flex flex-col mt-20 mb-10 gap-8 mx-[5%] md:mx-[10%] lg:mx-[25%] text-center">
+        <h1 className="font-bold text-xl md:text-3xl text-[#528fec]">{t('why')}</h1>
+        <p className="text-base text-[#2c2d41]">{t('whyDescription')}</p>
+        <h1 className="font-bold text-xl md:text-3xl text-[#528fec]">{t('ourAlignment')}</h1>
+        <p className="text-base text-[#2c2d41]">{t('ourAlignmentDescription')}</p>
+        <div className="flex flex-wrap p-5 justify-center gap-5 w-[350px] sm:w-[650px]">
+          <img src="/images/Goal 3.png" width={130} height={130} />
+          <img src="/images/Goal 4.png" width={130} height={130} />
+          <img src="/images/Goal 5.png" width={130} height={130} />
+          <img src="/images/Goal 6.png" width={130} height={130} />
+          <img src="/images/Goal 7.png" width={130} height={130} />
+          <img src="/images/Goal 8.png" width={130} height={130} />
+          <img src="/images/Goal 9.png" width={130} height={130} />
         </div>
-
-        <h1 className="vision">{t('vision')}</h1>
-
-        <p className="vision_details">{t('visionDescription')}</p>
+        <h1 className="font-bold text-xl md:text-3xl text-[#528fec]">{t('vision')}</h1>
+        <p className="text-base text-[#2c2d41]">{t('visionDescription')}</p>
       </div>
-      <div className="goals_container">
+      <div className="flex flex-wrap justify-evenly mb-10 gap-10">
         {GOALS.map((goal, idx) => (
           <GoalsCard
             key={'goals' + idx}
