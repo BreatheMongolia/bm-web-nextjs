ac73be9e6e4308b3f303874d32af361218e00ad8
Nara Davaasuren <94732814+Nara1469@users.noreply.github.com>
2023-11-08 19:58:41 -0700

    Fixed OurTeam section responsiveness

--- components/AboutUsPage/WhereOurTeamIsLocated.tsx
+++ components/AboutUsPage/WhereOurTeamIsLocated.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react'
 import WorldMap from 'assets/images/WorldMap'
-import Desktop from '../../components/Desktop'
-import Mobile from '../../components/Mobile'
 import UsFlag from 'assets/icons/Flags/US'
 import MongoliaFlag from 'assets/icons/Flags/Mongolia'
 import IndiaFlag from 'assets/icons/Flags/India'
@@ -17,41 +15,41 @@ import ChinaFlag from 'assets/icons/Flags/China'
 import { useTranslation } from 'next-i18next'
 
 const WhereOurTeamIsLocated: FC = () => {
-  const { t, i18n } = useTranslation('about')
+  const { t } = useTranslation('about')
 
   return (
     <>
-      <Desktop>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="where_our_team_title">{t('ourTeam.whereOurTeamIsLocated')}</h1>
+      {/* <Desktop> */}
+      <div className="hidden sm:flex flex-col items-center justify-center">
+        <h1 className="our_team_section_title">{t('ourTeam.whereOurTeamIsLocated')}</h1>
+        <div className="w-fit">
           <WorldMap />
         </div>
-      </Desktop>
-      <Mobile>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="where_our_team_title">{t('ourTeam.whereOurTeamIsLocated')}</h1>
-          <div className="flex flex-row items-center justify-center">
-            <MongoliaFlag />
-            <UsFlag />
-            <EUFlag />
-          </div>
-          <div className="flex flex-row items-center justify-center">
-            <InnerMongoliaFlag />
-            <CanadaFlag />
-            <UKFlag />
-          </div>
-          <div className="flex flex-row items-center justify-center">
-            <IndiaFlag />
-            <JapanFlag />
-            <TurkeyFlag />
-          </div>
-          <div className="flex flex-row items-center justify-center">
-            <AustraliaFlag />
-            <KoreaFlag />
-            <ChinaFlag />
-          </div>
+      </div>
+      {/* <Mobile> */}
+      <div className="sm:hidden flex flex-col items-center justify-center">
+        <h1 className="our_team_section_title">{t('ourTeam.whereOurTeamIsLocated')}</h1>
+        <div className="flex flex-row items-center justify-center">
+          <MongoliaFlag />
+          <UsFlag />
+          <EUFlag />
         </div>
-      </Mobile>
+        <div className="flex flex-row items-center justify-center">
+          <InnerMongoliaFlag />
+          <CanadaFlag />
+          <UKFlag />
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          <IndiaFlag />
+          <JapanFlag />
+          <TurkeyFlag />
+        </div>
+        <div className="flex flex-row items-center justify-center">
+          <AustraliaFlag />
+          <KoreaFlag />
+          <ChinaFlag />
+        </div>
+      </div>
     </>
   )
 }
