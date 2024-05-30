8a46ddc983f4e48b6cab41a0a4d74e3fb1518720
Temuulen Erdenekhuu <bg.genio@gmail.com>
2024-05-30 15:59:01 -0500

    minor: update menu with journalism link

--- components/layout/Header/navbar/navbar.tsx
+++ components/layout/Header/navbar/navbar.tsx
@@ -29,6 +29,7 @@ export const Navbar = () => {
         <div className="items-center gap-10 hidden lg:flex">
           <MenuItem title={t('home')} href="/" isActive={path == '/'} />
           <MenuItem title={t('takeAction')} href="/take-action" isActive={path.includes('action')} />
+          <MenuItem title={t('journalism')} href="https://www.ejfp.mn/" target={'_blank'} isActive={false} />
           <MenuItem title={t('news')} href="/news" isActive={path.includes('news')} />
           <MenuItem title={t('agaarNeg')} href="http://www.agaarneg.mn" target={'_blank'} isActive={false} />
           <MenuItem title={t('aboutUs')} href="/about/info" isActive={path.includes('about')} />
@@ -62,6 +63,7 @@ export const Navbar = () => {
       >
         <MenuItem title={t('home')} href="/" isActive={path == '/'} />
         <MenuItem title={t('takeAction')} href="/take-action" isActive={path.includes('take-action')} />
+        <MenuItem title={t('journalism')} href="https://www.ejfp.mn/" target={'_blank'} isActive={false} />
         <MenuItem title={t('news')} href="/news" isActive={path.includes('news')} />
         <MenuItem title={t('agaarNeg')} href="http://www.agaarneg.mn" target={'_blank'} isActive={false} />
         <MenuItem title={t('aboutUs')} href="/about" isActive={path.includes('about')} />
