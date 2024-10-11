39af6d22ebd35d6f5d9109e2df776c5b64a4cdd0
Tuguldur UlamUndrakh <rbktgldr@gmail.com>
2024-10-11 15:40:05 -0500

    Remove hidden class

--- pages/health/index.tsx
+++ pages/health/index.tsx
@@ -1,38 +1,35 @@
 import { H2 } from 'components/generic/Typography'
 import { NewsCarousel, TakeActionCarousel } from 'components/HomePage'
-import { parse } from 'date-fns'
 import { getHealthNews, getHealthPage } from 'lib/graphql-api/queries/health'
 import { getHomePage } from 'lib/graphql-api/queries/home'
-import { getNewsPosts } from 'lib/graphql-api/queries/news'
 import { getTranslated } from 'lib/utils/getTranslated'
 import { GetStaticProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
-import { text } from 'stream/consumers'
+
 
 const HealthPage = ({ featuredNews, featuredTakeActions, page, locale }) => {
-  // TODO: fix news carousel initial position
   return (
     <div className="container mx-auto flex flex-col pt-14">
       <H2 title={getTranslated(page?.title, page?.titleMn, locale)} />
       <p className='text-base text-zinc-800'>
-      {getTranslated(page?.body, page?.bodyMn, locale)}
+        {getTranslated(page?.body, page?.bodyMn, locale)}
       </p>
       <p className='text-zinc-500 mt-5 mb-5 '>
-      {getTranslated(page?.diagramTextTop, page?.diagramTextTopMn, locale)}
+        {getTranslated(page?.diagramTextTop, page?.diagramTextTopMn, locale)}
       </p>
 
       <iframe name="info-graphik-iframe" className='w-full h-[1000px] lg:h-[1375px]' id="serviceFrameSend" src={`https://health-effects.swisstph.ch/index.html?a_lang=` + locale}></iframe>
 
-      
+
       <div className='flex flex-col gap-20'>
-        <p className='font-semibold text-base text-zinc-800 mt-5 hidden lg:block'>
-        {getTranslated(page?.diagramTextBottom, page?.diagramTextBottomMn, locale)}
+        <p className='font-semibold text-base text-zinc-800 mt-5 lg:block'>
+          {getTranslated(page?.diagramTextBottom, page?.diagramTextBottomMn, locale)}
         </p>
         <TakeActionCarousel takeActionPosts={featuredTakeActions} locale={locale} />
         <NewsCarousel featuredNews={featuredNews} />
       </div>
-      
+
 
     </div>
   )
